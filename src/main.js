405918f3fa2384ee9e07883557df3bf7afe12af0
Arnaud Gaudin <arnaudgaudin86@gmail.com>
2025-11-27 11:29:43 +0100

    feat: handle active links

--- src/main.js
+++ src/main.js
@@ -3,3 +3,12 @@ import './style.css';
 
 // Génère tous les <i data-lucide="..."> en SVG
 createIcons({ icons });
+
+
+//gestion des liens actifs dans les menus
+document.addEventListener("DOMContentLoaded", () => {
+  const currentPath = window.location.pathname;
+  // Sélectionne tous les liens de navigation correspondant au chemin actuel
+  const links = Array.from(document.querySelectorAll("nav a")).filter(link => link.getAttribute("href") === currentPath);
+  links.forEach(link => link.classList.add("active"));
+});
