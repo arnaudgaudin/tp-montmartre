d577308a5d66ec4a859dacf02bdaf8bd87b0c305
Arnaud Gaudin <arnaudgaudin86@gmail.com>
2025-12-12 11:05:14 +0100

    feat: add options for npm build

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,19 @@
 import includeHtml from "vite-plugin-include-html";
 import { defineConfig } from "vite";
+import { resolve } from 'path'
 
 export default defineConfig({
   plugins: [includeHtml()],
+  build: {
+    rollupOptions: {
+      input: {
+        main: resolve(__dirname, 'index.html'),
+        presentation: resolve(__dirname, 'pages/presentation.html'),
+        blogMontmartre: resolve(__dirname, 'pages/blog-montmartre.html'),
+        contact: resolve(__dirname, 'pages/contact.html'),
+        politiqueConfidentialite: resolve(__dirname, 'pages/politique-confidentialite.html'),
+        mentionsLegales: resolve(__dirname, 'pages/mentions-legales.html'),
+      }
+    }
+  }
 });
